d5d88c8b18bf2a8213018567c89253e64e4f4c66
yash kelhe <yashkelhe30@gmail.com>
2025-01-16 11:50:57 +0530

    websockets

--- websockets/dist/index.js
+++ websockets/dist/index.js
@@ -34,15 +34,18 @@ const server = http_1.default.createServer(function (request, response) {
 });
 // create an one websockets server
 const wss = new ws_1.WebSocketServer({ server });
+let userCount = 0;
 wss.on("connection", function connection(ws) {
     ws.on("error", console.error);
     ws.on("message", function message(data, isBinary) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === ws_1.default.OPEN) {
+                // this will send the data to all the connected service
                 client.send(data, { binary: isBinary });
             }
         });
     });
+    console.log("use join the server ", ++userCount);
     ws.send("Hello! Message From Server!!");
 });
 server.listen(8080, function () {
